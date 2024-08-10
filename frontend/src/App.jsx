a3af1d727c0e29577c05fa3777870ca30069dab3
Your Name <yunish@gmail.com>
2024-08-10 09:11:24 +0545

    some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Home from "./components/Home.jsx";  // Ensure you import all components
-import Attractions from "./components/Attractions.jsx";
-import Gallery from "./components/Galary.jsx";
-import About from "./components/About.jsx";
-import Contact from "./components/Contact.jsx";
-import Community from "./components/Community.jsx";
-import RegisterGuide from "./components/RegisterGuide.jsx";
-import Guides from "./components/Guides.jsx";
+import Home from "./Pages/Home";
+// import Attractions from "./components/Attractions.jsx";
+// import Gallery from "./components/Galary.jsx";
+// import About from "./components/About.jsx";
+// import Contact from "./components/Contact.jsx";
+// import Community from "./components/Community.jsx";
+// import RegisterGuide from "./components/RegisterGuide.jsx";
+// import Guides from "./components/Guides.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/attractions" element={<Attractions />} />
+        {/* <Route path="/attractions" element={<Attractions />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/community" element={<Community />} />
         <Route path="/register-guide" element={<RegisterGuide />} />
-        <Route path="/guides" element={<Guides />} />
+        <Route path="/guides" element={<Guides />} /> */}
       </Routes>
     </BrowserRouter>
   );

58107d37e662fce9f86ab432720d12db2576cc47
Sangam208 <sangam.dhital72@gmail.com>
2024-08-10 11:58:22 +0545

    Uploaded image

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,151 @@
+import p1_img from "./Gorkha/palace.jpg";
+import p2_img from "./ChitwanNp/bengal.jpg";
+import p3_img from "./Dharan/DHARAN.jpg";
+import p4_img from "./Taplejung/image028.jpg";
+import p5_img from "./Pokhara/place.png";
+import p6_img from "./Patan/museum.jpg";
+import p7_img from "./Manang/img1.jpg";
+import p8_img from "./Mustang/adventure.jpg";
+import p9_img from "./Nagarkot/snowy.jpg";
+import p10_img from "./Lumbini/build.jpg";
+import p11_img from "./Khaptad/flower.jpg";
+import p12_img from "./Janakpur/sita.jpg";
+import p13_img from "./Ilam/ilam.jpeg";
+import p14_img from "./Bhaktapur/statue.jpg";
+
+
+
+
+let all_product =[
+    {
+        id: 1,
+        name: "Gorkha",
+        image: p1_img,
+        Rating: 4.0,
+        Review: 15
+
+    },
+
+    
+    {
+        id: 2,
+        name: "ChitwanNp",
+        image: p2_img,
+        Rating: 4.1,
+        Review: 13
+
+    },
+
+    {
+        id: 3,
+        name: "Dharan",
+        image: p3_img,
+        Rating: 4.2,
+        Review: 16
+
+    },
+
+    {
+        id: 4,
+        name: "Taplejung",
+        image: p4_img,
+        Rating: 4.1,
+        Review: 23
+
+    },
+
+    {
+        id: 5,
+        name: "Pokhara",
+        image: p5_img,
+        Rating: 4.5,
+        Review: 31
+
+    },
+
+    {
+        id: 6,
+        name: "Patan",
+        image: p6_img,
+        Rating: 3.8,
+        Review: 20
+
+    },
+
+    {
+        id: 7,
+        name: "Manang",
+        image: p7_img,
+        Rating: 3.8,
+        Review: 14
+
+    },
+
+    {
+        id: 8,
+        name: "Mustang",
+        image: p8_img,
+        Rating: 3.9,
+        Review: 21
+
+    },
+
+    {
+        id: 9,
+        name: "Nagarkot",
+        image: p9_img,
+        Rating: 4.2,
+        Review: 22
+
+    },
+
+    {
+        id: 10,
+        name: "Lumbini",
+        image: p10_img,
+        Rating: 4.1,
+        Review: 30
+
+    },
+
+    {
+        id: 11,
+        name: "Khaptad",
+        image: p11_img,
+        Rating: 4.3,
+        Review: 24
+
+    },
+
+    {
+        id: 12,
+        name: "Janakpur",
+        image: p12_img,
+        Rating: 4.0,
+        Review: 25
+
+    },
+
+    {
+        id: 13,
+        name: "Ilam",
+        image: p13_img,
+        Rating: 4.1,
+        Review: 21
+
+    },
+
+    {
+        id: 14,
+        name: "Bhaktapur",
+        image: p14_img,
+        Rating: 4.3,
+        Review: 27
+
+    },
+
+
+]
+
+
+export default all_product;

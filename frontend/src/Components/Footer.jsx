fc1128ecec41ae7aa700e85ecf96510c18a04244
Rojan Aryal <roozanaryal8@gmail.com>
2024-08-10 11:35:04 +0545

    Updated home

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,157 @@
+mport React from 'react'
+
+
+import logo from "../assets/logo1.png";
+import { FaYoutube } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+
+import { Link } from 'react-router-dom';
+export default function Footer() {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });}
+  return (
+    <div className='lg:px-10 xl:px-32 md:px-10 px-4 mt-32  bg-gradient-to-t pt-7 bg-primary text-white font-outfit'>
+    <div className=' flex md:justify-between md:flex-row flex-col justify-evenly  md:items-center items-start  mt-4'>
+      <div className='flex md:hidden justify-between w-full'>
+
+     
+    <div className=' md:w-[30%] w-[200px] md:hidden flex flex-col items-start '>
+      <div className="flex gap-3 items-center cursor-pointer " onClick={()=>{
+           
+          }}>
+            <img src={logo} alt="" className="lg:h-[60px] h-[50px]" />
+          </div>
+<p className='w-full font-light text-xs md:mt-14 mt-4 text-start '>Qualitätsleads für Photovoltaik – Ihr Schlüssel zum Erfolg. Willkommen bei ESK
+ </p>
+<div className='flex gap-3 justify-start items-start lg:mt-0 pt-6' >
+       {/* <div className='p-1 rounded-full  text-white cursor-pointer text-2xl'>
+       <FaFacebook/>
+       </div> */}
+       <a href='https://www.instagram.com/esk_leads?igsh=MWg1MXdkZGdtb2M4YQ%3D%3D&utm_source=qr' target='_blank' className='p-1 rounded-full  text-white cursor-pointer text-2xl'>
+       <FaInstagram/>
+       </a>
+       {/* <div className='p-1 rounded-full  text-white cursor-pointer text-2xl'>
+       <FaYoutube/>
+       </div>
+       <div className='p-1 rounded-full  text-white cursor-pointer text-2xl'>
+       <FaXTwitter/>
+       </div> */}
+      </div>
+      </div>
+      <div>
+      <h1 className='font-bold text-xl lg:mt-0 md:text-start text-start'>Über Uns</h1>
+     <div className=' flex flex-col lg:mt-0  space-y-4 pt-5 items-start  text-sm lg:text-base font-normal '>
+     <Link to="/" onClick={()=>{
+        handleScrollToTop()
+      }}>Startseite</Link>
+      <Link to="/über-uns" onClick={()=>{
+        handleScrollToTop()
+      }}>Über Uns</Link>
+      <Link to="/meeting-vereinbaren" onClick={()=>{
+        handleScrollToTop()
+      }}> Meeting vereinbaren</Link>
+      <Link to="/faq" onClick={()=>{
+        handleScrollToTop()
+      }}>FAQ</Link>
+     </div>
+      </div>
+      </div>
+      <div className='flex md:hidden justify-between mt-10'>
+      <div>
+      <h1 className='font-bold text-xl lg:mt-0 mt-4 md:text-start text-start'>Company</h1>
+     <div className=' flex flex-col lg:mt-0  space-y-4 pt-5 items-start text-sm lg:text-base font-normal '>
+     <Link className='cursor-pointer' to="/datenschutz">Datenschutz</Link>
+      <Link className='cursor-pointer' to="/impressum" onClick={()=>{
+        handleScrollToTop()
+      }}>Impressum</Link>
+      <Link to="/unsere-leads">Unsere Leads</Link>
+     
+     </div>
+      </div>
+      <div className='md:w-[20%] w-[50%]'>
+      <h1 className='font-bold text-xl lg:mt-0 mt-4 md:text-start text-start'>Kontaktieren Sie uns</h1>
+     <div className=' flex flex-col lg:mt-0  space-y-4 pt-5 md:items-start text-sm lg:text-base font-normal items-start'>
+     <p className='md:text-start text-start'>Siemensstraße 21, 84030 <br /> Landshut</p>
+<p>08171 97 46 23 23</p>
+<p>info@esk-direktvertrieb.de</p>
+     </div>
+      </div>
+      </div>
+     <div className=' md:w-[30%] w-[200px] md:flex hidden flex-col items-start '>
+      <div className="flex gap-3 items-center cursor-pointer" onClick={()=>{
+           
+          }}>
+            <img src={logo} alt="" className="lg:h-[60px] h-[50px]" />
+          </div>
+<p className='w-full font-light text-xs md:mt-14 mt-4 text-start '>Qualitätsleads für Photovoltaik – Ihr Schlüssel zum Erfolg. Willkommen bei ESK
+ </p>
+<div className='flex gap-3 justify-start items-start lg:mt-0 pt-6' >
+       {/* <div className='p-1 rounded-full  text-white cursor-pointer text-2xl '>
+       <FaFacebook/>
+       </div> */}
+       <a href='https://www.instagram.com/esk_leads?igsh=MWg1MXdkZGdtb2M4YQ%3D%3D&utm_source=qr' target='_blank' className='p-1 rounded-full  text-white cursor-pointer text-2xl'>
+       <FaInstagram/>
+       </a>
+       {/* <div className='p-1 rounded-full  text-white cursor-pointer text-2xl'>
+       <FaYoutube/>
+       </div> */}
+       {/* <div className='p-1 rounded-full  text-white cursor-pointer text-2xl'>
+       <FaXTwitter/>
+       </div> */}
+      </div>
+      </div>
+      <div className='md:block hidden'>
+      <h1 className='font-bold text-xl lg:mt-0 mt-4 md:text-start text-center'>Über Uns</h1>
+     <div className=' flex flex-col lg:mt-0  space-y-4 pt-5 md:items-start  text-sm lg:text-base font-normal items-center'>
+     <Link to="/" onClick={()=>{
+       handleScrollToTop()
+      }}>Startseite</Link>
+      <Link to="/über-uns" onClick={()=>{
+        handleScrollToTop()
+      }}>Über Uns</Link>
+      <Link to="/meeting-vereinbaren" onClick={()=>{
+        handleScrollToTop()
+      }}> Meeting vereinbaren</Link>
+     
+      <Link to="/faq" onClick={()=>{
+        handleScrollToTop()
+      }}>FAQ</Link>
+     </div>
+      </div>
+     
+      <div className='md:block -mt-6 hidden  '>
+      <h1 className='font-bold text-xl lg:mt-0 mt-4 md:text-start text-center'>Unternehmen</h1>
+     <div className=' flex flex-col lg:mt-0  space-y-4 pt-5 md:items-start text-sm lg:text-base font-normal items-center'>
+     <Link className='cursor-pointer' to="/datenschutz" onClick={()=>{
+        handleScrollToTop()
+      }}>Datenschutz</Link>
+      <Link className='cursor-pointer' to="/impressum" onClick={()=>{
+        handleScrollToTop()
+      }}>Impressum</Link>
+      <Link to="/unsere-leads">Unsere Leads</Link>
+      <p></p>
+     </div>
+      </div>
+      <div className='md:w-[20%] w-[50%] md:block hidden xl:-mt-5 mt-0'>
+      <h1 className='font-bold text-xl lg:mt-0 mt-4 md:text-start text-center'>Kontaktieren Sie uns</h1>
+     <div className=' flex flex-col lg:mt-0  space-y-4 pt-5 md:items-start text-sm lg:text-base font-normal items-center'>
+     <p className='md:text-start text-center'>Siemensstraße 21, 84030 <br /> Landshut</p>
+<p>08171 97 46 23 23</p>
+<p>info@esk-direktvertrieb.de
+</p>
+     </div>
+      </div>
+     
+     
+     
+   
+    </div>
+      <p className='mt-16 text-center font-medium md:text-base text-sm'>Copyright © ESK Will 2024. All Right Reserved</p>
+      </div>
+  )
+}
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,53 +1,142 @@
-import { FaSearch } from 'react-icons/fa';
-import Navbar from '../Components/Navbar.jsx';
-import gsap from 'gsap';
-import hackvideo from '../assets/hack.mp4';
-import { useEffect } from 'react';
+import { FaSearch } from "react-icons/fa";
+import Navbar from "../Components/Navbar.jsx";
+import gsap from "gsap";
+import hackvideo from "../assets/hack.mp4";
+import { useEffect } from "react";
+import Explore from "../assets/Explore.png";
 
 const Home = () => {
+   useEffect(() => {
+      const tl = gsap.timeline();
+      tl.fromTo(
+         ".wonder",
+         { opacity: 0, y: 40 }, // Start state
+         { opacity: 1, y: 0, duration: 0.5, delay: 1 } // End state
+      );
 
-    useEffect(() => {
-        const tl = gsap.timeline();
-        tl.fromTo(
-            '.wonder',
-            { opacity: 0, y: 40 }, // Start state
-            { opacity: 1, y: 0, duration: 0.5, delay: 1} // End state
-        );
+      tl.fromTo(
+         ".text",
+         { opacity: 0, y: 40 }, // Start state
+         { opacity: 1, y: 0, duration: 0.5, delay: 0.1 } // End state
+      );
+   }, []);
 
-        tl.fromTo('.text',
-            { opacity: 0, y: 40 }, // Start state
-            { opacity: 1, y: 0, duration: 0.5, delay: 0.1 } // End state
-        );
-    }, []); 
-
-    return (
-        <div className='relative h-screen w-screen overflow-hidden'>
-    
+   return (
+      <>
+         <div className="relative h-screen w-screen overflow-hidden">
             <div className="absolute top-0 left-0 w-full z-10">
-                <Navbar />
+               <Navbar />
             </div>
 
-            <video 
-                src={hackvideo} 
-                autoPlay 
-                muted 
-                loop 
-                className='h-full w-full object-cover'
+            <video
+               src={hackvideo}
+               autoPlay
+               muted
+               loop
+               className="h-full w-full object-cover"
             />
 
-            <div className='absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] text-center z-20 px-4 md:px-8 lg:px-16'>
-                <h1 className='wonder text-white text-2xl md:text-3xl lg:text-4xl mb-4'>Wonder More Worry Less</h1>
-                <div className='text flex flex-col sm:flex-row justify-center items-center'>
-                    <input 
-                        type='text' 
-                        placeholder='Find Your Next Adventure......' 
-                        className='p-3 border-white border-2 bg-transparent w-full sm:w-[21rem] text-slate-400 rounded-l'
-                    />
-                    <FaSearch className='search text-white text-xl sm:text-2xl ml-2 mt-2 sm:mt-0' />
-                </div>
+            <div className="absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] text-center z-20 px-4 md:px-8 lg:px-16">
+               <h1 className="wonder text-white text-2xl md:text-3xl lg:text-4xl mb-4">
+                  Wonder More Worry Less
+               </h1>
+               <div className="text flex flex-col sm:flex-row justify-center items-center">
+                  <input
+                     type="text"
+                     placeholder="Find Your Next Adventure......"
+                     className="p-3 border-white border-2 bg-transparent w-full sm:w-[21rem] text-slate-400 rounded-l"
+                  />
+                  <FaSearch className="search text-white text-xl sm:text-2xl ml-2 mt-2 sm:mt-0" />
+               </div>
             </div>
-        </div>
-    );
-}
+         </div>
+         {/* About US */}
+         <div className="flex p-4 mt-4 bg-white justify-center items-center">
+            <div className=" bg-orange-600 w-24 h-[3px]"></div>{" "}
+            <h1 className="text-black text-5xl font-semibold">
+               About <span className="font-semibold text-orange-600">Us</span>{" "}
+            </h1>
+            <div className=" bg-orange-600 w-24 h-[3px]"></div>
+         </div>
+         <div className="p-5 text-center">
+            <p>
+               Welcome to our travel company, your trusted guide to discovering
+               the wonders of Nepal. We specialize in helping tourists uncover
+               the hidden gems and must-see destinations across this
+               breathtaking country. From the towering peaks of the Himalayas to
+               the serene temples of Kathmandu, we ensure that your journey
+               through Nepal is filled with unforgettable experiences. Let us
+               guide you to the best spots, rich in culture, history, and
+               natural beauty, making your visit truly remarkable.
+            </p>
+         </div>
+
+         {/* services */}
+         <div className="flex p-3 justify-center p-4">
+            <div className="p-5 w-[25vw] ">
+               <img
+                  className="h-52 w-full object-cover  rounded-md"
+                  src={Explore}
+                  alt="photo"
+               />
+               <div>
+                  <h1 className=" font-bold">Explore Attraction</h1>
+                  <p>
+                     Ghandruk is a scenic village in Nepal's Annapurna region,
+                     offering stunning views of Annapurna and Machhapuchhre
+                     peaks.
+                  </p>
+               </div>
+            </div>
+            <div className="p-5 w-[25vw] ">
+               <img
+                  className="h-52 w-full object-cover  rounded-md"
+                  src={Explore}
+                  alt="photo"
+               />
+               <div>
+                  <h1 className=" font-bold">Explore Attraction</h1>
+                  <p>
+                     Ghandruk is a scenic village in Nepal's Annapurna region,
+                     offering stunning views of Annapurna and Machhapuchhre
+                     peaks.
+                  </p>
+               </div>
+            </div>
+            <div className="p-5 w-[25vw] ">
+               <img
+                  className="h-52 w-full object-cover  rounded-md"
+                  src={Explore}
+                  alt="photo"
+               />
+               <div>
+                  <h1 className=" font-bold">Explore Attraction</h1>
+                  <p>
+                     Ghandruk is a scenic village in Nepal's Annapurna region,
+                     offering stunning views of Annapurna and Machhapuchhre
+                     peaks.
+                  </p>
+               </div>
+            </div>
+         </div>
+
+         <div className="flex flex-col items-center">
+            <div className="">
+               <h1 className="text-4xl">
+                  <span className=" font-bold text-orange-600">What</span> our
+                  Client Say
+               </h1>
+            </div>
+            <div className="p-4">
+               <p className="text-center">
+                  Their expert guides showed me the best spots, including hidden
+                  gems I’d have missed. Everything was well-organized, making my
+                  journey stress-free and truly enjoyable. Highly recommended!
+               </p>
+            </div>
+         </div>
+      </>
+   );
+};
 
 export default Home;
